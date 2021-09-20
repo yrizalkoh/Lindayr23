932b5854f4a404ab6bee74a5ea59d68e26b8e0f8
Fajar Ghifari <fajarghifari3@gmail.com>
2021-09-20 22:32:54 +0700

    Disable text confirm in sweetalert

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', () => {
   Swal.fire({
     title: 'Are you sure to play music in background?',
-    text: "You won't be able to revert this!",
+    // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#3085d6',
