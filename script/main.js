2d96e4590cff66bd7fc70ec93bf26d8b58d795f9
yrizalkoh <101887183+yrizalkoh@users.noreply.github.com>
2022-04-04 11:25:48 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // Run when the webpage is loaded
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Bade di laguan in background moal?',
+    title: 'Are you sure to play music in background?',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
